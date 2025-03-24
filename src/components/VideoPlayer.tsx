ca9ccd3e2e22a266870ad307590d6c50a5a37ffd
Barbara <info@nonterritorial.foundation>
2025-03-24 10:10:21 +0100

    mux clean

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import MuxPlayer from "@mux/mux-player-react";
-import type { MuxPlayerProps } from "@mux/mux-player-react";
+
 
 type VideoPlayerProps = {
   playbackId: string;
